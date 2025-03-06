ce5817eae80966746adf219a40d62d233d02720f
AsgerAhmadov <AsgerAhmadov@gmail.com>
2025-03-06 22:07:25 +0400

    added create api

--- src/main/java/az/hamburg/librarymanagement/LibraryManagementApplication.java
+++ src/main/java/az/hamburg/librarymanagement/LibraryManagementApplication.java
@@ -2,8 +2,10 @@ package az.hamburg.librarymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "az.hamburg.librarymanagement.mappers")
 public class LibraryManagementApplication {
 
     public static void main(String[] args) {
